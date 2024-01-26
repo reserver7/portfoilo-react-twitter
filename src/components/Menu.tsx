c3c2a5e36a32f51b6c8bd40cf4bf090c4c21e402
reserver7 <reserver7@gmail.com>
2024-01-26 10:23:34 +0900

    feat: Firestore로 해시태그 검색하기

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@ import { BsHouse } from "react-icons/bs";
 import { BiUserCircle } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { MdLogout, MdLogin } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
 import { useContext } from "react";
 import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
@@ -22,6 +23,10 @@ export default function MenuList() {
         <button type="button" onClick={() => navigate("/profile")}>
           <BiUserCircle />
           Profile
+        </button>
+        <button type="button" onClick={() => navigate("/search")}>
+          <AiOutlineSearch />
+          Search
         </button>
         {user === null ? (
           <button type="button" onClick={() => navigate("/users/login")}>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,70 @@
-import React from "react";
+import PostBox from "components/posts/PostBox";
+import React, { useContext, useEffect, useState } from "react";
+import { PostProps } from "pages/home";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
 
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
+
+  const onChange = (e: any) => {
+    setTagQuery(e?.target?.value.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapshot) => {
+        let dataObj = snapshot?.docs?.map((doc) => ({
+          ...doc?.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <>
+            <div className="post__no-posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
