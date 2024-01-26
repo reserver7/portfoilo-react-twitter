989eb55dcda96eb030e8f62d48984cc56eb29550
reserver7 <reserver7@gmail.com>
2024-01-26 09:48:51 +0900

    feat: 해시태그 폼 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,7 @@
 import PostForm from "components/posts/PostForm";
 import PostBox from "components/posts/PostBox";
 import { useContext, useEffect, useState } from "react";
-import {
-  collection,
-  query,
-  where,
-  onSnapshot,
-  orderBy,
-} from "firebase/firestore";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import AuthContext from "context/AuthContext";
 import { db } from "firebaseApp";
 
@@ -21,6 +15,7 @@ export interface PostProps {
   likes?: string[];
   likeCount?: number;
   comments?: any;
+  hashTags?: string[];
 }
 
 export default function HomePage() {
